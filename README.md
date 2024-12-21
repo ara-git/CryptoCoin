0b35b8b6ad749128a5ad12a2de384b6b24ff77ac
ara-git <baribari.sanda@gmail.com>
2024-12-21 18:18:39 +0900

    add black-litterman logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Diversify the Crypto Coin Portfolio using Black-Litterman
+ref:
+https://www.nam.co.jp/market/column/quantstopics/2024/240802.html
--- src/black_litterman.py
+++ src/black_litterman.py
@@ -3,8 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class black_litterman:
-    def __init__(self, delta=2.5, tau=0.05):
+    def __init__(self, delta=2.5, tau=0.1625):
         self.delta = delta
         self.tau = tau
         pass
@@ -15,25 +20,87 @@ class black_litterman:
             "./data/intermediate/market_weight.csv", index_col=0
         )
         self.market_weight_df.sort_values(by="Name", inplace=True)
-        self.market_weight_array = np.array(self.market_weight_df["weight"])
+        self.market_weight_array = np.array(self.market_weight_df["weight"]).reshape(
+            [-1, 1]
+        )
 
         # 共分散行列を読み込む
-        self.cov_mat_df = pd.read_csv("./data/intermediate/cov_mat.csv", index_col=0)
-        self.cov_mat_array = np.array(self.cov_mat_df)
+        self.Sigma_df = pd.read_csv("./data/intermediate/cov_mat.csv", index_col=0)
+        self.Sigma_array = np.array(self.Sigma_df)
+
+        # 投資家のビューを読み込む
+        self.view_P_df = pd.read_csv("./data/intermediate/P_mat.csv", index_col=0)
+        self.view_P_array = np.array(self.view_P_df)
+
+        self.view_Q_df = pd.read_csv("./data/intermediate/Q_mat.csv", index_col=0)
+        self.view_Q_array = np.array(self.view_Q_df).reshape([-1, 1])
 
     def calculate_implied_return(self):
         # 均衡リターンを求める(reverse optimization)
-        self.implied_return_array = self.delta * np.dot(
-            self.cov_mat_array, self.market_weight_array
+        self.implied_return_array = (
+            self.delta * self.Sigma_array @ self.market_weight_array
         )
+        self.implied_return_array = self.implied_return_array.reshape(-1, 1)
+
+    def update_by_view(self):
+        """
+        均衡リターンに投資家のビューをブレンドし、事後リターン、事後分散を求める
+        """
+        # ビューの自身度合いを表す\Omegaを求める
+        Omega_list = []
+        for i in range(len(self.view_Q_df)):
+            Omega_list.append(
+                float(self.view_P_array[i] @ self.Sigma_array @ self.view_P_array[i].T)
+            )
+
+        self.Omega_array = np.diag(Omega_list)
+
+        # 事後リターンを求める
+        self.posterior_return = (
+            self.implied_return_array
+            + self.tau
+            * self.Sigma_array
+            @ self.view_P_array.T
+            @ np.linalg.inv(
+                self.tau * self.view_P_array @ self.Sigma_array @ self.view_P_array.T
+                + self.Omega_array
+            )
+            @ (self.view_Q_array - self.view_P_array @ self.implied_return_array)
+        )
+
+        # 事後共分散行列を求める
+        self.posterior_Sigma = (
+            self.Sigma_array
+            + self.tau * self.Sigma_array
+            - self.tau
+            * self.Sigma_array
+            @ self.view_P_array.T
+            @ np.linalg.inv(
+                self.tau * (self.view_P_array @ self.Sigma_array @ self.view_P_array.T)
+                + self.Omega_array
+            )
+            @ (self.tau * (self.view_P_array @ self.Sigma_array))
+        )
+
+        # 事後ウェイトを求める
+        self.posterior_weight_array = (
+            np.linalg.inv(self.delta * self.posterior_Sigma) @ self.posterior_return
+        )
+        self.posterior_weight_array /= sum(self.posterior_weight_array)
+
+    def output_result(self):
+        """
+        結果を出力する
+        """
+        # 重み
+        self.posterior_weight_df = self.market_weight_df.copy()
+        self.posterior_weight_df["weight"] = self.posterior_weight_array
+        self.posterior_weight_df.to_csv("./data/output/result_weight.csv")
 
 
 if __name__ == "__main__":
     ins = black_litterman()
     ins.read_data()
     ins.calculate_implied_return()
-
-    print("##")
-    print(ins.market_weight_df)
-    print(ins.cov_mat_df)
-    print(ins.implied_return_array)
+    ins.update_by_view()
+    ins.output_result()
